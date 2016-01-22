wido/ifupdown

commit 21f2c8863ad7a187877a5be4a4ecaffa12157585

Return an error when trying to ifup/ifdown/ifquery an unknown interface, unless it is ignored due to --allow or --exclude.

main.c
@@ -781,6 +781,23 @@ static bool do_interface(const char *target_iface) {
 	if(ignore_interface(iface))
 		return true;
 
+	/* Check if this interface exists */
+
+	bool found = false;
+
+	for (interface_defn *currif = defn->ifaces; currif; currif = currif->next) {
+		if (strcmp(liface, currif->logical_iface) == 0) {
+			found = true;
+			break;
+		}
+	}
+
+	if (!found) {
+		fprintf(stderr, "Unknown interface %s\n", liface);
+		return false;
+	}
+
+
 	/* Bail out if we are being called recursively on the same interface */
 
 	char envname[160];

commit 4135eca2a7f6dce77cb070225a48f6d0400feaa3

Allow ifdown/ifquery when physical interface is not same as logical interface.
Commit 21f2c8863ad7a187877a5be4a4ecaffa12157585 introduced a problem
when doing the following:

ifup foo=bar
ifdown foo

If there is no "iface foo" stanza in /e/n/i, the early check introduced
by that commit will cause ifdown to exit with an error. Fix this by
checking if there is a state file for the physical interface present.

main.c
@@ -812,6 +812,14 @@ static bool do_interface(const char *target_iface) {
 		}
 	}
 
+	if (cmds != iface_up) {
+		char filename[sizeof statefile + strlen(iface) + 2];
+		snprintf(filename, sizeof filename, "%s.%s", statefile, iface);
+		sanitize_file_name(filename + sizeof statefile);
+		if (access(filename, R_OK) == 0)
+			found = true;
+	}
+
 	if (!found) {
 		fprintf(stderr, "Unknown interface %s\n", liface);
 		return false;