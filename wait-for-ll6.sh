4022167a42e2fa01e68f0cb81b875559a346594b
Wido den Hollander <wido@widodh.nl>
2016-03-11 22:00:50 +0100

    ipv6: Wait properly for Link-Local Address to go through DAD
    The -tentative flag makes sure we only list addresses which are
    NOT in the process of Duplicate Address Detection.
    
    This makes sure we wait for the Link-Local Address to become available
    so that programs like dhclient can properly come online.
    
    This should fix Ubuntu bug #1543352
    
    Link: https://bugs.launchpad.net/ubuntu/+source/ifupdown/+bug/1543352
    Signed-off-by: Wido den Hollander <wido@widodh.nl>

--- wait-for-ll6.sh
+++ wait-for-ll6.sh
@@ -4,7 +4,7 @@ attempts=${IF_LL_ATTEMPTS:-60}
 delay=${IF_LL_INTERVAL:-0.1}
 
 for attempt in $(seq 1 $attempts); do
-	lladdress=$(ip -6 -o a s dev "$IFACE" scope link)
+	lladdress=$(ip -6 -o a s dev "$IFACE" scope link -tentative)
 	if [ -n "$lladdress" ]; then
 		attempt=0
 		break
